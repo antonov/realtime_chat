8b21453a47322a6d91f7002136f006ccc51439ff
antonov <artem@antonov.es>
2023-04-26 12:42:01 +0200

    Created a button, installed className utilities.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import Button from "@/app/components/Button";
-
 export default function Home() {
   return (
     <div className="text-rose-400">
-      Hello world!
-      <Button>hello</Button>
+      Dashboard
     </div>
   )
 }
