592cabb1b639873260c87599be7d897b7786250f
antonov <artem@antonov.es>
2023-04-27 14:13:14 +0200

    Add Friends Page and Form with validations.

---
+++ src/app/dashboard/add/page.tsx
@@ -0,0 +1,13 @@
+import AddFriendButton from "@/app/components/AddFriendButton";
+import exp from "constants";
+
+const AddFriendPage = () => {
+  return (
+    <main className="pt-8">
+      <h1 className="text-5xl font-bold mb-8">Add a Friend</h1>
+
+      <AddFriendButton />
+    </main>
+  )
+}
+export default AddFriendPage;
